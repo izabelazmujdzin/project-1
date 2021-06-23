ea31320ea6f141b728d715e90fef24ee38d86385
Izabela Żmujdzin <izabela.zmujdzin@gmail.com>
2021-06-23 10:49:32 +0200

    webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+
+import moment from "moment";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +8,56 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const firstName = "Iza";
+const age = 27;
+
+console.log(firstName);
+console.log(age);
+
+const emptyParagraph = document.querySelector(".section__paragraph--js");
+
+const greet = (firstName, age) => {
+	console.log(`Hej , nazywam się ${firstName} i mam ${age} lat `);
+};
+
+greet("Iza", age);
+
+function createContent(querySelectorContent, content) {
+	const element = document.querySelector(querySelectorContent);
+	element.innerHTML = content;
+}
+
+createContent(".section__paragraph--js", "Zanim to zapamiętam to miną wieki!");
+
+function helloWorld() {
+	console.log("Witaj świecie!");
+}
+helloWorld();
+
+const goodbye = (firstName, age) => {
+	console.log(
+		`Żegnam się z Wami, dziękuję, że byliście ${firstName}, ${age}`
+	);
+};
+
+goodbye("Iza", age);
+
+const button = document.querySelector(".action--js");
+
+button.addEventListener("click", () => {
+	const heading = document.querySelector(".section__header--js");
+	heading.innerHTML = `Hej! Nazywam się Iza  i mam 27 lat.`;
+});
+
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+	const nav = document.querySelector(".navigation--js");
+	nav.classList.toggle("navigation--open");
+});
+
+const startOfDay = moment().startOf("day").fromNow();
+
+const timePlaceholder = document.querySelector("time--js");
+
+timePlaceholder.innerHTML = startOfDay;
